86262092d656d92ee266f67fdfef78b9596f9abd
Sravani <sravaniramadugu123@gmail.com>
2026-09-24 19:38:13 +0530

    Connect frontend to deployed backend

--- frontend/app/predict/page.tsx
+++ frontend/app/predict/page.tsx
@@ -36,9 +36,10 @@ export default function PredictPage() {
       // -----------------------------
       // Send request to FastAPI
       // -----------------------------
-      const response = await fetch(
-        "http://127.0.0.1:8000/predict",
-        {
+     const API_URL =
+     process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
+
+     const response = await fetch(`${API_URL}/predict`, {
           method: "POST",
 
           headers: {
